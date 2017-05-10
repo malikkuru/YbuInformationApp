a8b5c8796b60138e673f64302411a254d6f1cbb4
malikkuru <mesihmalik@gmail.com>
2017-05-11 02:58:16 +0300

    Initial commit

---
+++ app/src/main/java/com/mesihmalikkuru/ybuinformationapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.mesihmalikkuru.ybuinformationapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    private TabLayout mTabLayout;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+}
